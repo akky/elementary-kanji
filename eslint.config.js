37ba009fa611f652bb4f3503a5374fab61f97072
Akky AKIMOTO <akky@users.noreply.github.com>
2025-06-07 16:12:31 +0900

    Fix ESLint globals for flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,22 @@
-export default [
-  {
-    ignores: ['**/findAndReplaceDOMText.js']
-  },
-  {
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module'
+module.exports = [
+    {
+        ignores: ['**/findAndReplaceDOMText.js']
     },
-    linterOptions: {
-      globals: {
-        chrome: 'readonly',
-        document: 'readonly',
-        window: 'readonly'
-      }
-    },
-    rules: {
-      indent: ['error', 4],
-      'linebreak-style': ['error', 'unix'],
-      quotes: [2, 'single', { avoidEscape: true }],
-      semi: ['error', 'always']
+    {
+        languageOptions: {
+            ecmaVersion: 2021,
+            sourceType: 'module',
+            globals: {
+                chrome: 'readonly',
+                document: 'readonly',
+                window: 'readonly'
+            }
+        },
+        rules: {
+            indent: ['error', 4],
+            'linebreak-style': ['error', 'unix'],
+            quotes: [2, 'single', { avoidEscape: true }],
+            semi: ['error', 'always']
+        }
     }
-  }
 ];
